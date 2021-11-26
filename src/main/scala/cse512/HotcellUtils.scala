76ffc6dcb4b312a3139af1a5d06ff07084a2512a
Asrita Mandalam <48210756+avmand@users.noreply.github.com>
2021-11-25 18:03:41 -0700

    Update HotcellUtils.scala

--- src/main/scala/cse512/HotcellUtils.scala
+++ src/main/scala/cse512/HotcellUtils.scala
@@ -47,5 +47,39 @@ object HotcellUtils {
     return calendar.get(Calendar.DAY_OF_MONTH)
   }
 
-  // YOU NEED TO CHANGE THIS PART
+  // YOU NEED TO CHANGE THIS PART   //CHANGED
+
+  def IsCellInBounds(x:Double, y:Double, z:Int, minX:Double, maxX:Double, minY:Double, maxY:Double, minZ:Int, maxZ:Int): Boolean =
+  {
+    if ( (x >= minX) && (x <= maxX) && (y >= minY) && (y <= maxY) && (z >= minZ) && (z <= maxZ) ){
+      return true
+    }
+    return false
+  }
+
+  def GetLocation(point: Int, minVal: Int, maxVal: Int) : Int={
+    if (point == minVal || point == maxVal){
+      return 1
+    }
+    else {
+      return 0
+    }
+  }
+
+  def GetCountOfNeighbors(minX:Int, minY:Int, minZ:Int, maxX:Int, maxY:Int, maxZ:Int, Xin:Int, Yin:Int, Zin:Int): Int ={
+    val whereInCube: Map[Int, String] = Map(0->"in", 1 -> "plane", 2-> "edge", 3-> "corner")
+    val howManyNeighbors: Map[String, Int] = Map("in" -> 26, "plane" -> 17, "edge" -> 11, "corner" -> 7)
+    var locVal = 0;
+    locVal += GetLocation(Xin, minX, maxX)
+    locVal += GetLocation(Yin, minY, maxY)
+    locVal += GetLocation(Zin, minZ, maxZ)
+    var whereInCubeForMap = whereInCube.get(locVal).get.toString()
+    return howManyNeighbors.get(whereInCubeForMap).get.toInt
+  }
+
+  def GetGScore(x: Int, y: Int, z: Int, numcells: Int, mean:Double, sd: Double, totalNeighbors: Int, sumAllNeighborsPoints: Int): Double ={
+    val num = (sumAllNeighborsPoints.toDouble - (mean*totalNeighbors.toDouble))
+    val denom = sd * math.sqrt((((numcells.toDouble * totalNeighbors.toDouble) - (totalNeighbors.toDouble * totalNeighbors.toDouble)) / (numcells.toDouble-1.0).toDouble).toDouble).toDouble
+    return (num/denom).toDouble
+  }
 }
