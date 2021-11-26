8dc8b24175636cc243baaa3911f42ad4aaea35ca
Asrita Mandalam <48210756+avmand@users.noreply.github.com>
2021-11-25 18:02:18 -0700

    Update HotcellAnalysis.scala

--- src/main/scala/cse512/HotcellAnalysis.scala
+++ src/main/scala/cse512/HotcellAnalysis.scala
@@ -42,8 +42,43 @@ def runHotcellAnalysis(spark: SparkSession, pointPath: String): DataFrame =
   val maxZ = 31
   val numCells = (maxX - minX + 1)*(maxY - minY + 1)*(maxZ - minZ + 1)
 
-  // YOU NEED TO CHANGE THIS PART
+  // YOU NEED TO CHANGE THIS PART   //CHANGED
 
-  return pickupInfo // YOU NEED TO CHANGE THIS PART
+  pickupInfo.createOrReplaceTempView("tempPickupInfo")
+
+  spark.udf.register("IsCellInBounds", (x: Double, y:Double, z:Int) =>  HotcellUtils.IsCellInBounds(x, y, z, minX, maxX, minY, maxY, minZ, maxZ))
+
+  val withinBoundaryPoints = spark.sql("select x,y,z from tempPickupInfo where IsCellInBounds(x, y, z) order by z,y,x").persist()
+  withinBoundaryPoints.createOrReplaceTempView("withinBoundaryPointsView")
+
+  val withinBoundaryPointsCount = spark.sql("select x,y,z,count(*) as numPoints from withinBoundaryPointsView group by z,y,x order by z,y,x").persist()
+  withinBoundaryPointsCount.createOrReplaceTempView("withinBoundaryPointsCountView")
+
+  spark.udf.register("square", (inputX: Int) => (inputX*inputX).toDouble)
+  val sumofPoints = spark.sql("select count(*), sum(numPoints), sum(square(numPoints)) as squaredSumOfAllPointsInGivenArea from withinBoundaryPointsCountView")
+
+  val sumPoints = sumofPoints.first().getLong(1) //sum x
+  val sumPointsSquared = sumofPoints.first().getDouble(2) //sum (x^2)
+
+  val meanXinFormula = sumPoints / numCells
+  val StandardDeviation = math.sqrt((sumPointsSquared / numCells) - (meanXinFormula * meanXinFormula) )
+
+  spark.udf.register("GetCountOfNeighbors", (minX: Int, minY: Int, minZ: Int, maxX: Int, maxY: Int, maxZ: Int, Xin: Int, Yin: Int, Zin: Int)
+  => ((HotcellUtils.GetCountOfNeighbors(minX, minY, minZ, maxX, maxY, maxZ, Xin, Yin, Zin))))
+  val NeighborVal = spark.sql("select viewWithinBoundCount1.x as x, viewWithinBoundCount1.y as y, viewWithinBoundCount1.z as z, GetCountOfNeighbors("+minX + "," + minY + "," + minZ + "," + maxX + "," + maxY + "," + maxZ + "," + "viewWithinBoundCount1.x,viewWithinBoundCount1.y,viewWithinBoundCount1.z) as totalNeighborVal, count(*) as neighborValWithValidPoints, sum(viewWithinBoundCount2.numPoints) as sumAllNeighborValPoints from withinBoundaryPointsCountView as viewWithinBoundCount1, withinBoundaryPointsCountView as viewWithinBoundCount2 where (viewWithinBoundCount2.x = viewWithinBoundCount1.x+1 or viewWithinBoundCount2.x = viewWithinBoundCount1.x or viewWithinBoundCount2.x = viewWithinBoundCount1.x-1) and (viewWithinBoundCount2.y = viewWithinBoundCount1.y+1 or viewWithinBoundCount2.y = viewWithinBoundCount1.y or viewWithinBoundCount2.y = viewWithinBoundCount1.y-1) and (viewWithinBoundCount2.z = viewWithinBoundCount1.z+1 or viewWithinBoundCount2.z = viewWithinBoundCount1.z or viewWithinBoundCount2.z = viewWithinBoundCount1.z-1) group by viewWithinBoundCount1.z, viewWithinBoundCount1.y, viewWithinBoundCount1.x order by viewWithinBoundCount1.z, viewWithinBoundCount1.y, viewWithinBoundCount1.x").persist()
+  NeighborVal.createOrReplaceTempView("NeighborValView")
+
+  spark.udf.register("GetGScore", (x: Int, y: Int, z: Int, numcells: Int, mean:Double, sd: Double, totalNeighborVal: Int, sumAllNeighborValPoints: Int) => ((
+    HotcellUtils.GetGScore(x, y, z, numcells, mean, sd, totalNeighborVal, sumAllNeighborValPoints))))
+  val NeighborValDecreasing = spark.sql("select x, y, z, " +
+    "GetGScore(x, y, z," +numCells+ ", " + meanXinFormula + ", " + StandardDeviation + ", totalNeighborVal, sumAllNeighborValPoints) as gi_statistic " +
+    "from NeighborValView " +
+    "order by gi_statistic desc")
+  NeighborValDecreasing.createOrReplaceTempView("NeighborValDecreasingView")
+  NeighborValDecreasing.show()
+
+  val result = spark.sql("select x,y,z from NeighborValDecreasingView")
+
+  return result
 }
 }
