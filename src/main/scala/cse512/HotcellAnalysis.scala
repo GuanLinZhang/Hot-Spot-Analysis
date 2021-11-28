10f8161fb9cd544b49ef68f2dcf954baff9f13d3
keerthi165 <srujanakeerthi90@gmail.com>
2021-11-28 00:44:15 -0700

    hot cell

--- src/main/scala/cse512/HotcellAnalysis.scala
+++ src/main/scala/cse512/HotcellAnalysis.scala
@@ -11,74 +11,85 @@ object HotcellAnalysis {
   Logger.getLogger("akka").setLevel(Level.WARN)
   Logger.getLogger("com").setLevel(Level.WARN)
 
-def runHotcellAnalysis(spark: SparkSession, pointPath: String): DataFrame =
-{
-  // Load the original data from a data source
-  var pickupInfo = spark.read.format("com.databricks.spark.csv").option("delimiter",";").option("header","false").load(pointPath);
-  pickupInfo.createOrReplaceTempView("nyctaxitrips")
-  pickupInfo.show()
+  def runHotcellAnalysis(spark: SparkSession, pointPath: String): DataFrame =
+  {
+    // Load the original data from a data source
+    var pickupInfo = spark.read.format("com.databricks.spark.csv").option("delimiter",";").option("header","false").load(pointPath);
+    pickupInfo.createOrReplaceTempView("nyctaxitrips")
+    pickupInfo.show()
 
-  // Assign cell coordinates based on pickup points
-  spark.udf.register("CalculateX",(pickupPoint: String)=>((
-    HotcellUtils.CalculateCoordinate(pickupPoint, 0)
-    )))
-  spark.udf.register("CalculateY",(pickupPoint: String)=>((
-    HotcellUtils.CalculateCoordinate(pickupPoint, 1)
-    )))
-  spark.udf.register("CalculateZ",(pickupTime: String)=>((
-    HotcellUtils.CalculateCoordinate(pickupTime, 2)
-    )))
-  pickupInfo = spark.sql("select CalculateX(nyctaxitrips._c5),CalculateY(nyctaxitrips._c5), CalculateZ(nyctaxitrips._c1) from nyctaxitrips")
-  var newCoordinateName = Seq("x", "y", "z")
-  pickupInfo = pickupInfo.toDF(newCoordinateName:_*)
-  pickupInfo.show()
+    // Assign cell coordinates based on pickup points
+    spark.udf.register("CalculateX",(pickupPoint: String)=>((
+      HotcellUtils.CalculateCoordinate(pickupPoint, 0)
+      )))
+    spark.udf.register("CalculateY",(pickupPoint: String)=>((
+      HotcellUtils.CalculateCoordinate(pickupPoint, 1)
+      )))
+    spark.udf.register("CalculateZ",(pickupTime: String)=>((
+      HotcellUtils.CalculateCoordinate(pickupTime, 2)
+      )))
+    pickupInfo = spark.sql("select CalculateX(nyctaxitrips._c5),CalculateY(nyctaxitrips._c5), CalculateZ(nyctaxitrips._c1) from nyctaxitrips")
+    var newCoordinateName = Seq("x", "y", "z")
+    pickupInfo = pickupInfo.toDF(newCoordinateName:_*)
+    pickupInfo.show()
 
-  // Define the min and max of x, y, z
-  val minX = -74.50/HotcellUtils.coordinateStep
-  val maxX = -73.70/HotcellUtils.coordinateStep
-  val minY = 40.50/HotcellUtils.coordinateStep
-  val maxY = 40.90/HotcellUtils.coordinateStep
-  val minZ = 1
-  val maxZ = 31
-  val numCells = (maxX - minX + 1)*(maxY - minY + 1)*(maxZ - minZ + 1)
+    // Define the min and max of x, y, z
+    val minX = -74.50/HotcellUtils.coordinateStep
+    val maxX = -73.70/HotcellUtils.coordinateStep
+    val minY = 40.50/HotcellUtils.coordinateStep
+    val maxY = 40.90/HotcellUtils.coordinateStep
+    val minZ = 1
+    val maxZ = 31
+    val numCells = (maxX - minX + 1)*(maxY - minY + 1)*(maxZ - minZ + 1)
 
-  // YOU NEED TO CHANGE THIS PART   //CHANGED
+    pickupInfo.createOrReplaceTempView("pickupInfoView")
+    pickupInfo = spark.sql("select x,y,z from pickupInfoView where x>= " + minX + " and x<= " + maxX + " and y>= " + minY + " and y<= " + maxY + " and z>= " + minZ + " and z<= " + maxZ + " order by z,y,x")
+    pickupInfo.createOrReplaceTempView("cells")
+    // pickupInfo.show()
 
-  pickupInfo.createOrReplaceTempView("tempPickupInfo")
+    pickupInfo = spark.sql("select x, y, z, count(*) as hotCells from cells group by x, y, z order by z,y,x")
+    pickupInfo.createOrReplaceTempView("selectedCellHotness")
+    // pickupInfo.show()
 
-  spark.udf.register("IsCellInBounds", (x: Double, y:Double, z:Int) =>  HotcellUtils.IsCellInBounds(x, y, z, minX, maxX, minY, maxY, minZ, maxZ))
+    val sumOfSelectedCcells = spark.sql("select sum(hotCells) as sumHotCells from selectedCellHotness")
+    sumOfSelectedCcells.createOrReplaceTempView("sumOfSelectedCcells")
+    // sumOfSelectedCcells.show()
 
-  val withinBoundaryPoints = spark.sql("select x,y,z from tempPickupInfo where IsCellInBounds(x, y, z) order by z,y,x").persist()
-  withinBoundaryPoints.createOrReplaceTempView("withinBoundaryPointsView")
+    val mean = (sumOfSelectedCcells.first().getLong(0).toDouble / numCells.toDouble).toDouble
+    // println(mean)
 
-  val withinBoundaryPointsCount = spark.sql("select x,y,z,count(*) as numPoints from withinBoundaryPointsView group by z,y,x order by z,y,x").persist()
-  withinBoundaryPointsCount.createOrReplaceTempView("withinBoundaryPointsCountView")
+    spark.udf.register("square", (x: Int) => (((x*x).toDouble)))
 
-  spark.udf.register("square", (inputX: Int) => (inputX*inputX).toDouble)
-  val sumofPoints = spark.sql("select count(*), sum(numPoints), sum(square(numPoints)) as squaredSumOfAllPointsInGivenArea from withinBoundaryPointsCountView")
+    val sumOfSquares = spark.sql("select sum(square(hotCells)) as sumOfSquares from selectedCellHotness")
+    sumOfSquares.createOrReplaceTempView("sumOfSquares")
+    // sumOfSquares.show()
 
-  val sumPoints = sumofPoints.first().getLong(1) //sum x
-  val sumPointsSquared = sumofPoints.first().getDouble(2) //sum (x^2)
+    val sd = scala.math.sqrt(((sumOfSquares.first().getDouble(0).toDouble / numCells.toDouble) - (mean.toDouble * mean.toDouble))).toDouble
+    // println(mean)
 
-  val meanXinFormula = sumPoints / numCells
-  val StandardDeviation = math.sqrt((sumPointsSquared / numCells) - (meanXinFormula * meanXinFormula) )
+    spark.udf.register("neighbouringCells", (inputX: Int, inputY: Int, inputZ: Int, minX: Int, maxX: Int, minY: Int, maxY: Int, minZ: Int, maxZ: Int) => ((HotcellUtils.calculateNeighbouringCells(inputX, inputY, inputZ, minX, minY, minZ, maxX, maxY, maxZ))))
 
-  spark.udf.register("GetCountOfNeighbors", (minX: Int, minY: Int, minZ: Int, maxX: Int, maxY: Int, maxZ: Int, Xin: Int, Yin: Int, Zin: Int)
-  => ((HotcellUtils.GetCountOfNeighbors(minX, minY, minZ, maxX, maxY, maxZ, Xin, Yin, Zin))))
-  val NeighborVal = spark.sql("select viewWithinBoundCount1.x as x, viewWithinBoundCount1.y as y, viewWithinBoundCount1.z as z, GetCountOfNeighbors("+minX + "," + minY + "," + minZ + "," + maxX + "," + maxY + "," + maxZ + "," + "viewWithinBoundCount1.x,viewWithinBoundCount1.y,viewWithinBoundCount1.z) as totalNeighborVal, count(*) as neighborValWithValidPoints, sum(viewWithinBoundCount2.numPoints) as sumAllNeighborValPoints from withinBoundaryPointsCountView as viewWithinBoundCount1, withinBoundaryPointsCountView as viewWithinBoundCount2 where (viewWithinBoundCount2.x = viewWithinBoundCount1.x+1 or viewWithinBoundCount2.x = viewWithinBoundCount1.x or viewWithinBoundCount2.x = viewWithinBoundCount1.x-1) and (viewWithinBoundCount2.y = viewWithinBoundCount1.y+1 or viewWithinBoundCount2.y = viewWithinBoundCount1.y or viewWithinBoundCount2.y = viewWithinBoundCount1.y-1) and (viewWithinBoundCount2.z = viewWithinBoundCount1.z+1 or viewWithinBoundCount2.z = viewWithinBoundCount1.z or viewWithinBoundCount2.z = viewWithinBoundCount1.z-1) group by viewWithinBoundCount1.z, viewWithinBoundCount1.y, viewWithinBoundCount1.x order by viewWithinBoundCount1.z, viewWithinBoundCount1.y, viewWithinBoundCount1.x").persist()
-  NeighborVal.createOrReplaceTempView("NeighborValView")
+    val neighbouringCells = spark.sql("select neighbouringCells(sch1.x, sch1.y, sch1.z, " + minX + "," + maxX + "," + minY + "," + maxY + "," + minZ + "," + maxZ + ") as neighbouringCellCount,"
+      + "sch1.x as x, sch1.y as y, sch1.z as z, "
+      + "sum(sch2.hotCells) as sumHotCells "
+      + "from selectedCellHotness as sch1, selectedCellHotness as sch2 "
+      + "where (sch2.x = sch1.x+1 or sch2.x = sch1.x or sch2.x = sch1.x-1) "
+      + "and (sch2.y = sch1.y+1 or sch2.y = sch1.y or sch2.y = sch1.y-1) "
+      + "and (sch2.z = sch1.z+1 or sch2.z = sch1.z or sch2.z = sch1.z-1) "
+      + "group by sch1.z, sch1.y, sch1.x "
+      + "order by sch1.z, sch1.y, sch1.x")
+    neighbouringCells.createOrReplaceTempView("neighbouringCells")
+    // neighbouringCells.show()
 
-  spark.udf.register("GetGScore", (x: Int, y: Int, z: Int, numcells: Int, mean:Double, sd: Double, totalNeighborVal: Int, sumAllNeighborValPoints: Int) => ((
-    HotcellUtils.GetGScore(x, y, z, numcells, mean, sd, totalNeighborVal, sumAllNeighborValPoints))))
-  val NeighborValDecreasing = spark.sql("select x, y, z, " +
-    "GetGScore(x, y, z," +numCells+ ", " + meanXinFormula + ", " + StandardDeviation + ", totalNeighborVal, sumAllNeighborValPoints) as gi_statistic " +
-    "from NeighborValView " +
-    "order by gi_statistic desc")
-  NeighborValDecreasing.createOrReplaceTempView("NeighborValDecreasingView")
-  NeighborValDecreasing.show()
+    spark.udf.register("g_score", (neighbouringCellCount: Int, sumHotCells: Int, numCells: Int, x: Int, y: Int, z: Int, mean: Double, sd: Double) => ((HotcellUtils.calculateGScore(neighbouringCellCount, sumHotCells, numCells, x, y, z, mean, sd))))
 
-  val result = spark.sql("select x,y,z from NeighborValDecreasingView")
+    pickupInfo = spark.sql("select g_score(neighbouringCellCount, sumHotCells, "+ numCells + ", x, y, z," + mean + ", " + sd + ") as getisOrdStatistic, x, y, z from neighbouringCells order by getisOrdStatistic desc");
+    pickupInfo.createOrReplaceTempView("g_score")
+    // pickupInfo.show()
 
-  return result
-}
+    pickupInfo = spark.sql("select x, y, z from g_score")
+    pickupInfo.createOrReplaceTempView("finalPickupInfo")
+    // pickupInfo.show()
+    return pickupInfo
+  }
 }
--- src/main/scala/cse512/HotcellUtils.scala
+++ src/main/scala/cse512/HotcellUtils.scala
@@ -47,39 +47,38 @@ object HotcellUtils {
     return calendar.get(Calendar.DAY_OF_MONTH)
   }
 
-  // YOU NEED TO CHANGE THIS PART   //CHANGED
-
-  def IsCellInBounds(x:Double, y:Double, z:Int, minX:Double, maxX:Double, minY:Double, maxY:Double, minZ:Int, maxZ:Int): Boolean =
+  def calculateNeighbouringCells(inputX: Int, inputY: Int, inputZ: Int, minX: Int, maxX: Int, minY: Int, maxY: Int, minZ: Int, maxZ: Int): Int =
   {
-    if ( (x >= minX) && (x <= maxX) && (y >= minY) && (y <= maxY) && (z >= minZ) && (z <= maxZ) ){
-      return true
+    var count = 0
+
+    if (inputX == minX || inputX == maxX) {
+      count += 1
     }
-    return false
+
+    if (inputY == minY || inputY == maxY) {
+      count += 1
+    }
+
+    if (inputZ == minZ || inputZ == maxZ) {
+      count += 1
+    }
+
+    if (count == 1) {
+      return 17;
+    } else if (count == 2) {
+      return 11;
+    } else if (count == 3) {
+      return 7;
+    }
+
+    return 26;
   }
 
-  def GetLocation(point: Int, minVal: Int, maxVal: Int) : Int={
-    if (point == minVal || point == maxVal){
-      return 1
-    }
-    else {
-      return 0
-    }
-  }
+  def calculateGScore(adjacentCellCount: Int, sumHotCells: Int, numCells: Int, x: Int, y: Int, z: Int, mean: Double, standardDeviation: Double): Double =
+  {
+    val dividend = (sumHotCells.toDouble - (mean * adjacentCellCount.toDouble))
+    val divisor = standardDeviation * math.sqrt((((numCells.toDouble * adjacentCellCount.toDouble) - (adjacentCellCount.toDouble * adjacentCellCount.toDouble)) / (numCells.toDouble - 1.0).toDouble).toDouble).toDouble
 
-  def GetCountOfNeighbors(minX:Int, minY:Int, minZ:Int, maxX:Int, maxY:Int, maxZ:Int, Xin:Int, Yin:Int, Zin:Int): Int ={
-    val whereInCube: Map[Int, String] = Map(0->"in", 1 -> "plane", 2-> "edge", 3-> "corner")
-    val howManyNeighbors: Map[String, Int] = Map("in" -> 26, "plane" -> 17, "edge" -> 11, "corner" -> 7)
-    var locVal = 0;
-    locVal += GetLocation(Xin, minX, maxX)
-    locVal += GetLocation(Yin, minY, maxY)
-    locVal += GetLocation(Zin, minZ, maxZ)
-    var whereInCubeForMap = whereInCube.get(locVal).get.toString()
-    return howManyNeighbors.get(whereInCubeForMap).get.toInt
-  }
-
-  def GetGScore(x: Int, y: Int, z: Int, numcells: Int, mean:Double, sd: Double, totalNeighbors: Int, sumAllNeighborsPoints: Int): Double ={
-    val num = (sumAllNeighborsPoints.toDouble - (mean*totalNeighbors.toDouble))
-    val denom = sd * math.sqrt((((numcells.toDouble * totalNeighbors.toDouble) - (totalNeighbors.toDouble * totalNeighbors.toDouble)) / (numcells.toDouble-1.0).toDouble).toDouble).toDouble
-    return (num/denom).toDouble
+    return (dividend / divisor).toDouble
   }
 }
